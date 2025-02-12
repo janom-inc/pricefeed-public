d2b05bf3bf9a64ddfeaf6d2ead13298c91254c45
Vis Virial <visvirial@gmail.com>
2025-02-12 23:53:02 +0900

    Retrun Response*.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import React from 'react';
 import {
-	Box,
-	Tab,
-	Tabs,
-	Container,
-	Stack,
-	Link,
-} from "@mui/material";
+	useState,
+	useEffect,
+} from 'react';
+import { PriceFeed } from '@pricefeed/sdk';
 
 export default function Home() {
+	
+	const [prices, setPrices] = useState<Record<string, number>>({});
+	
+	useEffect(() => {
+		const pf = new PriceFeed();
+	}, []);
 	return (
 		<div>
-			Home
+			<img alt="eyecatch" src="/img/eyecatch.png" style={{ width: '100%' }} />
 		</div>
 	);
 }
--- sdk/ts/src/lib/PriceFeed.ts
+++ sdk/ts/src/lib/PriceFeed.ts
@@ -6,23 +6,20 @@ import {
 import {
 	Pair,
 	PathElement,
-	Path,
-	Route,
-	ExchangeRate,
+	ResponseRate,
 	ResponseRates,
+	ResponseRoute,
 	ResponsePrice,
 	TypedEventTarget,
 } from '../types';
 
 export class PriceFeed extends (EventTarget as typeof TypedEventTarget<{
-	rate: MessageEvent<ExchangeRate>;
+	rates: MessageEvent<ResponseRates>;
+	price: MessageEvent<ResponsePrice>;
 }>) {
 	
 	public readonly rest = new PriceFeedRest(this.restEndpoint);
 	public readonly ws = new PriceFeedWebSocket(this.wsEndpoint);
-	
-	// sourceAndPairStr => ExchangeRate.
-	private _rates: Map<string, ExchangeRate> = new Map();
 	
 	constructor(
 		public readonly restEndpoint: string = process.env.PRICEFEED_REST_ENDPOINT ?? 'https://api.pricefeed.info',
@@ -30,10 +27,12 @@ export class PriceFeed extends (EventTarget as typeof TypedEventTarget<{
 	) {
 		super();
 		this.ws.addEventListener('message-rates', (event) => {
-			const rates: ExchangeRate[] = event.data.data.payload;
-			rates.forEach(rate => {
-				this._setRate(rate);
-			});
+			const rates = event.data;
+			this.dispatchEvent(new MessageEvent('rates', { data: rates }));
+		});
+		this.ws.addEventListener('message-price', (event) => {
+			const price = event.data;
+			this.dispatchEvent(new MessageEvent('price', { data: price }));
 		});
 	}
 	
@@ -41,30 +40,24 @@ export class PriceFeed extends (EventTarget as typeof TypedEventTarget<{
 		this.ws.close();
 	}
 	
-	public getCachedRate(elem: PathElement): ExchangeRate | null {
-		return this._rates.get(elem.toString()) ?? null;
+	public async fetchRate(elem: PathElement): Promise<ResponseRate> {
+		return await this.rest.rate(elem.source, elem.pair);
 	}
 	
-	private _setRate(rate: ExchangeRate) {
-		this._rates.set(new PathElement(rate.source, rate.pair).toString(), rate);
-		this.dispatchEvent(new MessageEvent('rate', { data: rate }));
+	public async watchRates(elems: Set<PathElement>): Promise<ResponseRates> {
+		return await this.ws.subscribeRates([...elems.values()].map((sourceAndPair) => sourceAndPair.toString()));
 	}
 	
-	public async fetchRate(elem: PathElement): Promise<ExchangeRate> {
-		const rate = (await this.rest.rate(elem.source, elem.pair)).data.payload;
-		this._setRate(rate);
-		return rate;
+	public async fetchRoute(pair: Pair): Promise<ResponseRoute> {
+		return await this.rest.route(pair);
 	}
 	
-	public async watchRates(elems: Set<PathElement>): Promise<ExchangeRate[]> {
-		const res = await this.ws.subscribeRates([...elems.values()].map((sourceAndPair) => sourceAndPair.toString()));
-		return res.data.payload;
+	public async fetchPrice(pair: Pair): Promise<ResponsePrice> {
+		return await this.rest.price(pair);
 	}
 	
-	public async fetchRoute(pair: Pair): Promise<Route> {
-		const pathStrs: string[] = (await this.rest.route(pair)).data.payload;
-		const paths = pathStrs.map(pathStr => Path.fromString(pathStr));
-		return new Set(paths);
+	public async watchPrice(pair: Pair): Promise<ResponsePrice> {
+		return await this.ws.subscribePrice(pair.toString());
 	}
 	
 }
